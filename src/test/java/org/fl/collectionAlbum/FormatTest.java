239ca3c27d100a97eae88af91175dfe6f35e00fd
Frederic Lefevre <frederic.lefevre@fr.ibm.com>
2019-06-06 17:28:58 +0200

    More test

--- src/test/java/org/fl/collectionAlbum/FormatTest.java
+++ src/test/java/org/fl/collectionAlbum/FormatTest.java
@@ -11,10 +11,10 @@ import com.google.gson.JsonParser;
 
 class FormatTest {
 
+	private final static Logger logger = Logger.getLogger(FormatTest.class.getName());
+	
 	@Test
-	void test() {
-		
-		Logger logger = Logger.getLogger(FormatTest.class.getName());
+	void test1() {
 		
 		String formatStr1 = "{}" ;
 		JsonObject jf1 = new JsonParser().parse(formatStr1).getAsJsonObject();
@@ -24,4 +24,46 @@ class FormatTest {
 
 	}
 
+	@Test
+	void test2() {
+		
+		String formatStr1 = "{\"cd\": 3 }" ;
+		JsonObject jf1 = new JsonParser().parse(formatStr1).getAsJsonObject();
+		Format format1 = new Format(jf1, logger) ;
+		
+		assertEquals(3, format1.getPoids()) ;
+
+	}
+	
+	@Test
+	void test3() {
+		
+		String formatStr1 = "{\"cd\": 2 , \"45t\" : 1 }" ;
+		JsonObject jf1 = new JsonParser().parse(formatStr1).getAsJsonObject();
+		Format format1 = new Format(jf1, logger) ;
+		
+		assertEquals(2.5, format1.getPoids()) ;
+
+	}
+	
+	@Test
+	void test4() {
+		
+		String formatStr1 = "{\"33t\": 2 , \"45t\" : 1 }" ;
+		JsonObject jf1 = new JsonParser().parse(formatStr1).getAsJsonObject();
+		Format format1 = new Format(jf1, logger) ;
+		
+		String formatStr2 = "{\"cd\": 2 , \"45t\" : 1 }" ;
+		JsonObject jf2 = new JsonParser().parse(formatStr2).getAsJsonObject();
+		Format format2 = new Format(jf2, logger) ;
+		
+		Format format3 = new Format(null, logger) ;
+		assertEquals(0, format3.getPoids()) ;
+		
+		format3.incrementFormat(format1);
+		assertEquals(format1.getPoids(), format3.getPoids()) ;
+		
+		format3.incrementFormat(format2);
+		assertEquals(format1.getPoids() + format2.getPoids(), format3.getPoids()) ;
+	}
 }
