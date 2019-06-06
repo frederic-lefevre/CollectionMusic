726a96ff56cc67328f05350aeb35f4bb3ec27aba
Frederic Lefevre <frederic.lefevre@fr.ibm.com>
2019-06-06 18:20:59 +0200

    Refactor

--- src/main/java/org/fl/collectionAlbumGui/CollectionAlbumGui.java
+++ src/main/java/org/fl/collectionAlbumGui/CollectionAlbumGui.java
@@ -12,19 +12,10 @@ import org.fl.collectionAlbumGui.entry.ConcertEntryPane;
 
 public class CollectionAlbumGui  extends JFrame {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 8726429353709418534L;
 
 	private static final String DEFAULT_PROP_FILE = "file:///C:/FredericPersonnel/musique/RapportCollection/albumCollection.properties" ;
 	
-	// Logger
-	private Logger albumLog ;
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -42,7 +33,7 @@ public class CollectionAlbumGui  extends JFrame {
 		
 		// init logger and parameters
 		Control.initControl(DEFAULT_PROP_FILE) ;
-   		albumLog = Control.getAlbumLog() ;
+		Logger albumLog = Control.getAlbumLog() ;
    		
    		// init main window
    		setBounds(50, 50, 1500, 1000);
@@ -50,18 +41,17 @@ public class CollectionAlbumGui  extends JFrame {
 		setTitle("Collection d'album") ;
 		
 		// init panel de lecture et génération de site
-		GenerationPane gPane = new GenerationPane(albumLog) ;
-		AlbumEntryPane aPane = new AlbumEntryPane() ;
+		GenerationPane gPane   = new GenerationPane(albumLog) ;
+		AlbumEntryPane aPane   = new AlbumEntryPane() ;
 		ConcertEntryPane cPane = new ConcertEntryPane() ;
 		
 		JTabbedPane operationTab = new JTabbedPane() ;
-		operationTab.addTab("Génération", gPane.getGenPane()) ;
-		operationTab.addTab("Entrée album", aPane.getaEntryPane()) ;
+		operationTab.addTab("Génération",     gPane.getGenPane()) ;
+		operationTab.addTab("Entrée album",   aPane.getaEntryPane()) ;
 		operationTab.addTab("Entrée concert", cPane.getcEntryPane()) ;
 		
 		getContentPane().add(operationTab) ;
-		pack() ;
-		
+		pack() ;		
 	}
 
 }
