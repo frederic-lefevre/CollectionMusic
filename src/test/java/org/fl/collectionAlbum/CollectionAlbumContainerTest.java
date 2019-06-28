93a05c569ba80880586cc3492bfe3bad7e9282aa
Frederic Lefevre <frederic.lefevre@fr.ibm.com>
2019-06-28 11:34:42 +0200

    More tests

--- src/test/java/org/fl/collectionAlbum/CollectionAlbumContainerTest.java
+++ src/test/java/org/fl/collectionAlbum/CollectionAlbumContainerTest.java
@@ -11,12 +11,18 @@ class CollectionAlbumContainerTest {
 	private static final Logger logger = Logger.getLogger(CollectionAlbumContainerTest.class.getName()) ;
 	
 	@Test
-	void test() {
+	void testEmptyContainer() {
 		
 		CollectionAlbumContainer albumsContainer = CollectionAlbumContainer.getEmptyInstance(logger) ;
 		
 		assertEquals(0, albumsContainer.getCollectionAlbumsMusiques().getNombreAlbums()) ;
 		assertEquals(0, albumsContainer.getConcerts().getNombreConcerts()) ;
+		assertEquals(0, albumsContainer.getCollectionArtistes().getNombreArtistes()) ;
+		assertEquals(0, albumsContainer.getConcertsArtistes().getNombreArtistes()) ;
+		assertNotNull(albumsContainer.getCalendrierArtistes()) ;
+		assertNull(albumsContainer.getArtisteKnown("Toto", "Titi")) ;
+		assertNotNull(albumsContainer.getStatChronoComposition()) ;
+		assertNotNull(albumsContainer.getStatChronoEnregistrement()) ;
 	}
 
 }
