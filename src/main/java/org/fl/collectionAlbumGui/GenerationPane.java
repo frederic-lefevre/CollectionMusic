399e7ed661d1fc1e2d0276b740c242ceac60013b
Frederic Lefevre <frederic.lefevre@fr.ibm.com>
2019-07-14 09:13:59 +0200

    Formatting

--- src/main/java/org/fl/collectionAlbumGui/GenerationPane.java
+++ src/main/java/org/fl/collectionAlbumGui/GenerationPane.java
@@ -7,26 +7,24 @@ import javax.swing.JPanel;
 
 public class GenerationPane {
 
-	private JPanel genPane ;
+	private JPanel 		 genPane ;
 	private StartControl startButton ;
 	private StartControl genButton ;
 	
-	private final static String rText = "Lecture des fichiers albums et concerts" ;
-	private final static String gText = "Génération du nouveau site collection" ;
-	private final static String iText = "Arrêt";
-	private final static String sText = "Aucune collection lue" ;
+	private final static String rText  = "Lecture des fichiers albums et concerts" ;
+	private final static String gText  = "Génération du nouveau site collection" ;
+	private final static String iText  = "Arrêt";
+	private final static String sText  = "Aucune collection lue" ;
 	private final static String s1Text = "Aucun site généré" ;
 	
 	public GenerationPane(Logger albumLog) {
-		
-		
+			
 		genPane = new JPanel() ;
 		genPane.setLayout(new BoxLayout(genPane, BoxLayout.Y_AXIS));
 		
 		startButton = new StartControl(rText, iText, sText) ;
 		genPane.add(startButton.getProcCtrl()) ;
-		
-		
+				
 		genButton = new StartControl(gText, iText, s1Text) ;
 		genButton.deactivate() ;
 		genPane.add(genButton.getProcCtrl()) ;
@@ -42,8 +40,5 @@ public class GenerationPane {
 		genButton.getStartButton().addActionListener(sg) ;
 	} 
 
-	public JPanel getGenPane() {
-		return genPane;
-	}
-
+	public JPanel getGenPane() { return genPane;	}
 }
