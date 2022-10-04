711b879016f8c14b8c0fde3224e7e4b166f4c118
Frederic Lefevre <frederic.bn.lefevre@googlemail.com>
2022-10-04 07:33:53 +0200

    Add json video constant

--- src/main/java/org/fl/collectionAlbum/JsonMusicProperties.java
+++ src/main/java/org/fl/collectionAlbum/JsonMusicProperties.java
@@ -40,10 +40,13 @@ public class JsonMusicProperties {
 	public final static String TICKET_IMG	  = "imageTicket" ;
 	public final static String MORCEAUX		  = "morceaux" ;
 	public final static String AUDIO_FILE	  = "audioFiles" ;
+	public final static String VIDEO_FILE	  = "videoFiles" ;
 	public final static String TYPE		  	  = "type" ;
 	public final static String SOURCE		  = "source" ;
 	public final static String BIT_DEPTH	  = "bitDepth" ;
 	public final static String BIT_RATE		  = "bitRate" ;
 	public final static String SAMPLING_RATE  = "samplingRate" ;
+	public final static String VIDEO_WIDTH	  = "width" ;
+	public final static String VIDEO_HEIGHT	  = "height" ;
 
 }
