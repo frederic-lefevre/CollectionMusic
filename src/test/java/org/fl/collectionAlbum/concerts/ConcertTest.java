77d6e8ede81fa84ba3f9bca3dacbd065c43bdabb
Frederic Lefevre <frederic.bn.lefevre@googlemail.com>
2023-08-29 18:05:58 +0200

    Migrate to assertj

--- src/test/java/org/fl/collectionAlbum/concerts/ConcertTest.java
+++ src/test/java/org/fl/collectionAlbum/concerts/ConcertTest.java
@@ -24,7 +24,7 @@ SOFTWARE.
 
 package org.fl.collectionAlbum.concerts;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -48,7 +48,7 @@ class ConcertTest {
 		LieuxDesConcerts lieuxDesConcerts = new LieuxDesConcerts() ;
 		Concert concert = new Concert(new JsonObject(), lla, lieuxDesConcerts) ;
 		
-		assertNotNull(concert);
+		assertThat(concert).isNotNull();
 	}
 	
 	private static final String concertStr1 = """
@@ -66,40 +66,46 @@ class ConcertTest {
 
 	@Test
 	void testConcert1() {
-		
+
 		JsonObject jConcert = JsonParser.parseString(concertStr1).getAsJsonObject();
 
-		ListeArtiste la = new ListeArtiste() ;
-		List<ListeArtiste> lla = new ArrayList<ListeArtiste>() ;
-		lla.add(la) ;
-		LieuxDesConcerts lieuxDesConcerts = new LieuxDesConcerts() ;
-		
-		Concert concert = new Concert(jConcert, lla, lieuxDesConcerts) ;
-		
-		LieuConcert juan = concert.getLieuConcert() ;
-		assertEquals("Juan-les-Pins, Alpes-Maritimes", juan.getLieu()) ;
-		
-		assertEquals(0, juan.getNombreConcert()) ;
-		assertEquals(1, concert.getTicketImages().size()) ;
-		assertEquals("/Annees1990/1990/07_Juillet/RayCharles01.jpg", concert.getTicketImages().get(0)) ;
-		
+		ListeArtiste la = new ListeArtiste();
+		List<ListeArtiste> lla = new ArrayList<ListeArtiste>();
+		lla.add(la);
+		LieuxDesConcerts lieuxDesConcerts = new LieuxDesConcerts();
+
+		Concert concert = new Concert(jConcert, lla, lieuxDesConcerts);
+
+		LieuConcert juan = concert.getLieuConcert();
+		assertThat(juan.getLieu()).isEqualTo("Juan-les-Pins, Alpes-Maritimes");
+
+		assertThat(juan.getNombreConcert()).isZero();
+
+		assertThat(concert.getTicketImages())
+			.singleElement()
+			.isEqualTo("/Annees1990/1990/07_Juillet/RayCharles01.jpg");
+
 		juan.addConcert(concert);
-		assertEquals(1, juan.getNombreConcert()) ;
-		
-		List<Artiste> lDeeDee = concert.getAuteurs() ;
-		
-		assertEquals(1, lDeeDee.size()) ;
-		
-		Artiste deeDee = lDeeDee.get(0) ;
-		assertEquals("Dee Dee", deeDee.getPrenoms()) ;
-		assertEquals("Bridgewater", deeDee.getNom()) ;
-		assertEquals(0, deeDee.getNbAlbum()) ;
-		assertEquals(0, deeDee.getNbConcert()) ;
-		
-		concert.addMusicArtfactArtistesToList(la);
-		assertEquals(1, deeDee.getNbConcert()) ;
-		
-		assertEquals(concert, deeDee.getConcerts().getConcerts().get(0)) ;
-		
+		assertThat(juan.getNombreConcert()).isEqualTo(1);
+
+		List<Artiste> lDeeDee = concert.getAuteurs();
+
+		assertThat(lDeeDee)
+			.singleElement()
+			.satisfies(deeDee -> {
+				
+				assertThat(deeDee.getPrenoms()).isEqualTo("Dee Dee");
+				assertThat(deeDee.getNom()).isEqualTo("Bridgewater");
+				assertThat(deeDee.getNbAlbum()).isZero();
+				assertThat(deeDee.getNbConcert()).isZero();
+				
+				concert.addMusicArtfactArtistesToList(la);
+				assertThat(deeDee.getNbConcert()).isEqualTo(1);
+				
+				assertThat(deeDee.getConcerts().getConcerts())
+					.singleElement()
+					.isEqualTo(concert);
+			});
+
 	}
 }
