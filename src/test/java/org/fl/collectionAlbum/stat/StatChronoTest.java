76e46a173e2ebc50453eca2eaad3cc02b60cbd87
Frederic Lefevre <frederic.bn.lefevre@googlemail.com>
2023-08-31 07:52:44 +0200

    Migrate to assertj

--- src/test/java/org/fl/collectionAlbum/stat/StatChronoTest.java
+++ src/test/java/org/fl/collectionAlbum/stat/StatChronoTest.java
@@ -1,6 +1,6 @@
 package org.fl.collectionAlbum.stat;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.temporal.TemporalAccessor;
 
@@ -12,36 +12,35 @@ class StatChronoTest {
 	@Test
 	void test() {
 		
-		StatChrono sc1 = new StatChrono() ;
+		StatChrono sc1 = new StatChrono();
 		
-		assertEquals("0", sc1.getStatForYear(0)) ;
-		assertEquals("0", sc1.getStatForYear(1969)) ;
-		assertEquals("0", sc1.getStatForDecennie(1969)) ;	
+		assertThat(sc1.getStatForYear(0)).isEqualTo("0");
+		assertThat(sc1.getStatForYear(1969)).isEqualTo("0");
+		assertThat(sc1.getStatForDecennie(1969)).isEqualTo("0");	
 
-		assertEquals(0, sc1.getStatAnnuelle().size()) ;
-		assertEquals(0, sc1.getStatDecennale().size()) ;
-		assertEquals(0, sc1.getStatSiecle().size()) ;
+		assertThat(sc1.getStatAnnuelle()).isNotNull().isEmpty();
+		assertThat(sc1.getStatDecennale()).isNotNull().isEmpty();
+		assertThat(sc1.getStatSiecle()).isNotNull().isEmpty();
 		
-		String d1 = "1969-09-03" ;
-		TemporalAccessor ta1 = TemporalUtils.parseDate(d1) ;
-		String d2 = "1960-01-01" ;
-		TemporalAccessor ta2 = TemporalUtils.parseDate(d2) ;
-		String d3 = "1969-12-31" ;
-		TemporalAccessor ta3 = TemporalUtils.parseDate(d3) ;
-		String d4 = "1970-01-01" ;
-		TemporalAccessor ta4 = TemporalUtils.parseDate(d4) ;
+		String d1 = "1969-09-03";
+		TemporalAccessor ta1 = TemporalUtils.parseDate(d1);
+		String d2 = "1960-01-01";
+		TemporalAccessor ta2 = TemporalUtils.parseDate(d2);
+		String d3 = "1969-12-31";
+		TemporalAccessor ta3 = TemporalUtils.parseDate(d3);
+		String d4 = "1970-01-01";
+		TemporalAccessor ta4 = TemporalUtils.parseDate(d4);
 
-		sc1.AddAlbum(ta1, 1.5) ;
-		sc1.AddAlbum(ta2, 1) ;
-		sc1.AddAlbum(ta3, 2) ;
-		sc1.AddAlbum(ta4, 5) ;
-		assertEquals("3.5", sc1.getStatForYear(1969)) ;
-		assertEquals("1",   sc1.getStatForYear(1960)) ;
-		assertEquals("4.5", sc1.getStatForDecennie(1960)) ;
-		assertEquals("4.5", sc1.getStatForDecennie(1965)) ;
-		assertEquals("5",   sc1.getStatForYear(1970)) ;
-		assertEquals("5",   sc1.getStatForDecennie(1970)) ;
-
+		sc1.AddAlbum(ta1, 1.5);
+		sc1.AddAlbum(ta2, 1);
+		sc1.AddAlbum(ta3, 2);
+		sc1.AddAlbum(ta4, 5);
+		assertThat(sc1.getStatForYear(1969)).isEqualTo("3.5");
+		assertThat(sc1.getStatForYear(1960)).isEqualTo("1");
+		assertThat(sc1.getStatForDecennie(1960)).isEqualTo("4.5");
+		assertThat(sc1.getStatForDecennie(1965)).isEqualTo("4.5");
+		assertThat(sc1.getStatForYear(1970)).isEqualTo("5");
+		assertThat(sc1.getStatForDecennie(1970)).isEqualTo("5");
 	}
 
 }
