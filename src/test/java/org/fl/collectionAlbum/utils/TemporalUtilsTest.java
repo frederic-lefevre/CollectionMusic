63fdf6798bfd5c548f8dbeecd521a611b92a3a19
Frederic Lefevre <frederic.bn.lefevre@googlemail.com>
2023-08-31 08:15:35 +0200

    Complete migration to assertj

--- src/test/java/org/fl/collectionAlbum/utils/TemporalUtilsTest.java
+++ src/test/java/org/fl/collectionAlbum/utils/TemporalUtilsTest.java
@@ -1,6 +1,7 @@
 package org.fl.collectionAlbum.utils;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import java.time.format.DateTimeParseException;
 import java.time.temporal.ChronoField;
@@ -14,55 +15,48 @@ class TemporalUtilsTest {
 	@Test
 	void test() {
 		
-		String d1 = "1962-09-03" ;
-		TemporalAccessor ta1 = TemporalUtils.parseDate(d1) ;
+		String d1 = "1962-09-03";
+		TemporalAccessor ta1 = TemporalUtils.parseDate(d1);
 		
-		assertEquals(3, ta1.get(ChronoField.DAY_OF_MONTH)) ;
-		assertEquals(9, ta1.get(ChronoField.MONTH_OF_YEAR)) ;
-		assertEquals(1962, ta1.get(ChronoField.YEAR)) ;
+		assertThat(ta1.get(ChronoField.DAY_OF_MONTH)).isEqualTo(3);
+		assertThat(ta1.get(ChronoField.MONTH_OF_YEAR)).isEqualTo(9);
+		assertThat(ta1.get(ChronoField.YEAR)).isEqualTo(1962);
 	}
 
 	@Test
 	void test2() {
 		
-		String d1 = "2019-06-16" ;
-		TemporalAccessor ta1 = TemporalUtils.parseDate(d1) ;
+		String d1 = "2019-06-16";
+		TemporalAccessor ta1 = TemporalUtils.parseDate(d1);
 		
-		assertEquals(16, ta1.get(ChronoField.DAY_OF_MONTH)) ;
-		assertEquals(6, ta1.get(ChronoField.MONTH_OF_YEAR)) ;
-		assertEquals(2019, ta1.get(ChronoField.YEAR)) ;
+		assertThat(ta1.get(ChronoField.DAY_OF_MONTH)).isEqualTo(16);
+		assertThat(ta1.get(ChronoField.MONTH_OF_YEAR)).isEqualTo(6);
+		assertThat(ta1.get(ChronoField.YEAR)).isEqualTo(2019);
 		
-		String dRes1 = TemporalUtils.formatDate(ta1) ;
-		assertEquals("16 juin 2019", dRes1) ;
+		String dRes1 = TemporalUtils.formatDate(ta1);
+		assertThat(dRes1).isEqualTo("16 juin 2019");
 	}
 	
 	@Test
 	void test3() {
 		
-		try {
-			TemporalUtils.parseDate("2019-00-16") ;
-			assertTrue(false) ;
-		} catch (DateTimeParseException e) {
-			assertTrue(true) ;
-		}
+		assertThatExceptionOfType(DateTimeParseException.class)
+			.isThrownBy(() -> TemporalUtils.parseDate("2019-00-16"));
 	}
 	
 	@Test
 	void test4() {
 		
-		String d1 = "2016-01" ;
-		TemporalAccessor ta1 = TemporalUtils.parseDate(d1) ;
+		String d1 = "2016-01";
+		TemporalAccessor ta1 = TemporalUtils.parseDate(d1);
 		
-		try {
-			assertEquals(1, ta1.get(ChronoField.DAY_OF_MONTH)) ;
-			assertTrue(false) ;
-		} catch (UnsupportedTemporalTypeException e) {
-			assertTrue(true) ;
-		}
-		assertEquals(1, ta1.get(ChronoField.MONTH_OF_YEAR)) ;
-		assertEquals(2016, ta1.get(ChronoField.YEAR)) ;
+		assertThatExceptionOfType(UnsupportedTemporalTypeException.class)
+			.isThrownBy(() -> ta1.get(ChronoField.DAY_OF_MONTH));
+
+		assertThat(ta1.get(ChronoField.MONTH_OF_YEAR)).isEqualTo(1);
+		assertThat(ta1.get(ChronoField.YEAR)).isEqualTo(2016);
 		
-		String dRes1 = TemporalUtils.formatDate(ta1) ;
-		assertEquals("janvier 2016", dRes1) ;
+		String dRes1 = TemporalUtils.formatDate(ta1);
+		assertThat(dRes1).isEqualTo("janvier 2016");
 	}
 }
