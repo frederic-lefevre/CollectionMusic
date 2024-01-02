9293cd2c5ea4fd3d68ad5c9f42392297cd4dbbaf
Frederic Lefevre <frederic.bn.lefevre@googlemail.com>
2024-01-02 10:04:51 +0100

    Fix tab pane title

--- src/main/java/org/fl/collectionAlbumGui/GenerationPane.java
+++ src/main/java/org/fl/collectionAlbumGui/GenerationPane.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
 
-Copyright (c) 2017, 2023 Frederic Lefevre
+Copyright (c) 2017, 2024 Frederic Lefevre
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -126,7 +126,7 @@ public class GenerationPane extends JPanel {
 		discogsReleasesScrollPane.setPreferredSize(new Dimension(1800,700));
 		discogsReleasesPane.add(discogsReleasesScrollPane);
 		
-		collectionTabPanes.add(discogsReleasesPane, "Discos releases");
+		collectionTabPanes.add(discogsReleasesPane, "Discogs releases");
 		
 		
 		add(collectionTabPanes);
