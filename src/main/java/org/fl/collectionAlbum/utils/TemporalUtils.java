c0ecad83bee201c1f76284339d9bde2f108fc4ff
Frederic Lefevre <frederic.bn.lefevre@googlemail.com>
2024-05-23 14:19:53 +0200

    Add methods

--- src/main/java/org/fl/collectionAlbum/utils/TemporalUtils.java
+++ src/main/java/org/fl/collectionAlbum/utils/TemporalUtils.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
 
-Copyright (c) 2017, 2023 Frederic Lefevre
+Copyright (c) 2017, 2024 Frederic Lefevre
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -25,49 +25,123 @@ SOFTWARE.
 package org.fl.collectionAlbum.utils;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.Year;
 import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.time.format.ResolverStyle;
+import java.time.temporal.ChronoField;
 import java.time.temporal.TemporalAccessor;
 import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class TemporalUtils {
 
    	// Pattern pour les dates (parse et format)
-   	private final static String datePatternParse  = "uuuu[-MM[-dd]]" ;  	
-   	private final static String datePatternFormat = "[[dd ]MMMM] uuuu" ;
-   	private final static String yearPatternFormat = "uuuu" ;
+   	private static final String datePatternParse  = "uuuu[-MM[-dd]]";  	
+   	private static final String datePatternFormat = "[[dd ]MMMM] uuuu";
+   	private static final String yearPatternFormat = "uuuu";
    	
-   	private final static DateTimeFormatter dateTimeParser    = DateTimeFormatter.ofPattern(datePatternParse,  Locale.FRANCE).withResolverStyle(ResolverStyle.STRICT) ;
-   	private final static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(datePatternFormat, Locale.FRANCE).withResolverStyle(ResolverStyle.STRICT) ;
-   	private final static DateTimeFormatter yearTimeFormatter = DateTimeFormatter.ofPattern(yearPatternFormat, Locale.FRANCE).withResolverStyle(ResolverStyle.STRICT) ;
+   	private static final DateTimeFormatter dateTimeParser    = DateTimeFormatter.ofPattern(datePatternParse,  Locale.FRANCE).withResolverStyle(ResolverStyle.STRICT);
+   	private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(datePatternFormat, Locale.FRANCE).withResolverStyle(ResolverStyle.STRICT);
+   	private static final DateTimeFormatter yearTimeFormatter = DateTimeFormatter.ofPattern(yearPatternFormat, Locale.FRANCE).withResolverStyle(ResolverStyle.STRICT);
 
 	// Parse a date and try to return a LocalDate first, if not possible a YearMonth, if not possible a Year
     public static TemporalAccessor parseDate(String d) {
     	
     	if ((d == null) || (d.isEmpty())) {
-    		return null ;
+    		return null;
     	} else {
-    		return dateTimeParser.parseBest(d, LocalDate::from, YearMonth::from, Year::from) ;
+    		return dateTimeParser.parseBest(d, LocalDate::from, YearMonth::from, Year::from);
     	}
     }
     
     public static String formatDate(TemporalAccessor d) {
     	
        	if (d == null) {
-    		return "" ;
+    		return "";
        	} else {
-    		return dateTimeFormatter.format(d) ;
+    		return dateTimeFormatter.format(d);
     	}	
     }
     
    public static String formatYear(TemporalAccessor d) {
     	
        	if (d == null) {
-    		return "" ;
+    		return "";
        	} else {
-    		return yearTimeFormatter.format(d) ;
+    		return yearTimeFormatter.format(d);
     	}	
-    }  
+    }
+   
+	// (Try to) compare 2 TemporalAccessors
+	public static int compareTemporal(TemporalAccessor t1, TemporalAccessor t2, Logger log) {
+		
+		if (t1 == null) {
+			if (t2 == null) return 0 ;
+			return 1 ;
+		} else if (t2 == null) {
+			return -1 ;
+		} else {
+			try {
+				
+				LocalDateTime d1 = getRoundedLocalDateTime(t1);
+				LocalDateTime d2 = getRoundedLocalDateTime(t2);
+				
+				if ((d1 != null) && (d2 != null)) {
+					return d1.compareTo(d2) ;
+				} else {
+					log.severe("Cannot convert to LocalDate comparing 2 TemporalAccessor ");
+					return 0 ;
+				}
+				
+			} catch (Exception e) {
+				log.log(Level.SEVERE, "Exception comparing 2 TemporalAccessor ", e);
+				return 0;
+			}
+		}
+	}
+	
+	public static LocalDateTime getRoundedLocalDateTime(TemporalAccessor t) {
+		
+		if (t instanceof LocalDateTime) {
+			return (LocalDateTime)t;
+		} else if (t instanceof LocalDate) {
+			return ((LocalDate)t).atStartOfDay();
+		} else if (t instanceof YearMonth) {
+			return ((YearMonth)t).atDay(1).atStartOfDay();
+		} else if (t instanceof Year) {
+			return ((Year)t).atDay(1).atStartOfDay();
+		} else {
+			// try to get (some) TemporalField manually 
+						
+			if (t.isSupported(ChronoField.YEAR)) {
+				
+				int year =  t.get(ChronoField.YEAR);
+				
+				int month 		 = getRoundedField(t, ChronoField.MONTH_OF_YEAR);
+				int day 		 = getRoundedField(t, ChronoField.DAY_OF_MONTH);
+				int hour 		 = getRoundedField(t, ChronoField.HOUR_OF_DAY);
+				int minute 		 = getRoundedField(t, ChronoField.MINUTE_OF_HOUR);
+				int second 		 = getRoundedField(t, ChronoField.SECOND_OF_MINUTE);
+				int nanoOfsecond = getRoundedField(t, ChronoField.NANO_OF_SECOND);
+				
+				return LocalDateTime.of(year, month, day, hour, minute,second, nanoOfsecond);
+				
+			} else {
+				return null;
+			}
+		}
+
+	}
+   
+   private static int getRoundedField(TemporalAccessor t, ChronoField c) {
+   	
+   	if (t.isSupported(c)) {
+   		return t.get(c);
+   	} else {
+   		return (int)c.range().getMinimum();
+   	}
+   }
 }
