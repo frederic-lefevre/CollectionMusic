81851628a148aa18e8ea8edc0a9a4b33f07086e4
Frederic Lefevre <frederic.bn.lefevre@googlemail.com>
2024-05-23 14:07:37 +0200

    Move FuzzyPeriod class

---
+++ src/main/java/org/fl/collectionAlbum/utils/FuzzyPeriod.java
@@ -0,0 +1,70 @@
+/*
+ * MIT License
+
+Copyright (c) 2017, 2024 Frederic Lefevre
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package org.fl.collectionAlbum.utils;
+
+import java.time.temporal.TemporalAccessor;
+import java.util.logging.Logger;
+
+import org.fl.util.date.TemporalUtils;
+
+public class FuzzyPeriod {
+
+	private static final Logger logger =  Logger.getLogger(FuzzyPeriod.class.getName());
+	
+	private final TemporalAccessor debut ;
+   	private final TemporalAccessor fin ;
+   	
+   	private boolean isValid ;
+	
+	public FuzzyPeriod(TemporalAccessor d, TemporalAccessor f) {
+		
+		debut 	= d ;
+		fin   	= f ;
+		isValid = true ;
+		
+		if ((debut != null) && (fin != null)) {
+			if (TemporalUtils.compareTemporal(debut, fin, logger) > 0) {
+				isValid = false ;
+				logger.warning("Période de dates invalides (début après fin)") ;
+			}
+		} else {
+			isValid = false ;
+			logger.warning("Période de dates invalides (début ou fin invalides)") ;
+		}
+	}
+
+	public TemporalAccessor getDebut() {
+		return debut;
+	}
+
+	public TemporalAccessor getFin() {
+		return fin;
+	}
+
+	public boolean isValid() {
+		return isValid ;
+	}
+
+}
