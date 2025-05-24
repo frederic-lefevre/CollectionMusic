0fe57730a4700df92ca723d3234d4cb0c4ef2a7d
Frederic Lefevre <frederic.bn.lefevre@googlemail.com>
2025-05-24 21:56:37 +0200

    Test album with audio and video

--- src/test/java/org/fl/collectionAlbum/json/migrator/AlbumVersionMigrator4Test.java
+++ src/test/java/org/fl/collectionAlbum/json/migrator/AlbumVersionMigrator4Test.java
@@ -215,6 +215,116 @@ class AlbumVersionMigrator4Test {
 		
 	}
 	
+	private static final String albumStr4 = """
+{
+  "titre" : "Electric ladyland",
+  "format" : {
+    "cd" : 3,
+    "bluerayMixed" : 1,
+    "audioFiles" : [ {
+      "bitDepth" : 16,
+      "samplingRate" : 44.1,
+      "source" : "CD",
+      "type" : "FLAC",
+      "note" : "Remaster Bernie Grundman. Son compressé. Qualité inferieure",
+      "location" : [ "E:\\\\Musique\\\\h\\\\Jimi Hendrix\\\\Electric Ladyland\\\\Electric Ladyland (50th, CD, 2018)" ]
+    }, {
+      "bitDepth" : 24,
+      "samplingRate" : 96,
+      "source" : "Bluray 2.0",
+      "type" : "FLAC",
+      "note" : "Remaster Bernie Grundman. Son compressé. Qualité inferieure",
+      "location" : [ "E:\\\\Musique\\\\h\\\\Jimi Hendrix\\\\Electric Ladyland\\\\Electric Ladyland [Bluray 2_0, 2018 24-96]" ]
+    }, {
+      "bitDepth" : 24,
+      "samplingRate" : 176.4,
+      "source" : "Bluray 5.1",
+      "type" : "FLAC",
+      "note" : "Remaster Bernie Grundman, Remix Eddie Kramer",
+      "location" : [ "E:\\\\Musique\\\\h\\\\Jimi Hendrix\\\\Electric Ladyland\\\\Electric Ladyland [Bluray 5_1, 2018, 24 176]" ]
+    }, {
+      "bitDepth" : 16,
+      "samplingRate" : 44.1,
+      "source" : "CD",
+      "type" : "FLAC",
+      "note" : "Live At The Hollywood Bowl - Electric Ladyland 50th Anniversary",
+      "location" : [ "E:\\\\Musique\\\\h\\\\Jimi Hendrix\\\\Live At The Hollywood Bowl" ]
+    }, {
+      "bitDepth" : 16,
+      "samplingRate" : 44.1,
+      "source" : "CD",
+      "type" : "FLAC",
+      "note" : "At Last - The Beginning - The Making Of Electric Ladyland - 50th Anniversary Deluxe Edition",
+      "location" : [ "E:\\\\Musique\\\\h\\\\Jimi Hendrix\\\\The Making Of Electric Ladyland" ]
+    } ],
+    "videoFiles" : [ {
+      "width" : 720,
+      "height" : 480,
+      "source" : "Blu-ray",
+      "type" : "M2TS",
+      "location" : [ "G:\\\\Video\\\\Musique\\\\h\\\\Jimi Hendrix\\\\Electric Ladyland 1968" ]
+    } ]
+  },
+  "rangement" : "33T",
+  "notes" : [ "50th anniversary edition", "Release LC 02361, bar code 1 90758 59022 6, Experience Hendrix 2018, Made in E.U", "Encore un son compressé, qualité inférieure pour le CD et le Bluray version 2.0 96Khz/24bits", "Remaster Bernie Grundman" ],
+  "auteurCompositeurs" : [ {
+    "nom" : "Hendrix",
+    "prenom" : "Jimi"
+  } ],
+  "enregistrement" : [ "1967-05-06", "1968-09-14" ],
+  "jsonVersion" : 3,
+  "discogs" : "12793152",
+  "discogsFormatValidation" : true
+}
+			""";
+	
+	
+	@Test
+	void shouldMigrateAlbum3() throws JsonMappingException, JsonProcessingException {
+		
+		JsonNode formatJson = assertAndGetFormatJson(albumStr4);
+		
+		JsonNode jsonAudioFiles = formatJson.get(JsonMusicProperties.AUDIO_FILE);
+		assertThat(jsonAudioFiles).isNotNull();
+		assertThat(jsonAudioFiles.size()).isEqualTo(5);
+		
+		assertThat(jsonAudioFiles).satisfiesExactlyInAnyOrder(
+				jsonAudioFile -> { 
+					JsonNode locationsJson = jsonAudioFile.get( JsonMusicProperties.LOCATION);
+					assertThat(locationsJson).isNotNull().singleElement().satisfies(locationJson -> 
+						assertThat(locationJson.asText()).isEqualTo("h/Jimi Hendrix/Electric Ladyland/Electric Ladyland (50th, CD, 2018)/"));					
+				},
+				jsonAudioFile -> {
+					JsonNode locationsJson = jsonAudioFile.get( JsonMusicProperties.LOCATION);
+					assertThat(locationsJson).isNotNull().singleElement().satisfies(locationJson -> 
+						assertThat(locationJson.asText()).isEqualTo("h/Jimi Hendrix/Electric Ladyland/Electric Ladyland [Bluray 2_0, 2018 24-96]/"));		
+				},
+				jsonAudioFile -> {
+					JsonNode locationsJson = jsonAudioFile.get( JsonMusicProperties.LOCATION);
+					assertThat(locationsJson).isNotNull().singleElement().satisfies(locationJson -> 
+						assertThat(locationJson.asText()).isEqualTo("h/Jimi Hendrix/Electric Ladyland/Electric Ladyland [Bluray 5_1, 2018, 24 176]/"));		
+				},
+				jsonAudioFile -> {
+					JsonNode locationsJson = jsonAudioFile.get( JsonMusicProperties.LOCATION);
+					assertThat(locationsJson).isNotNull().singleElement().satisfies(locationJson -> 
+						assertThat(locationJson.asText()).isEqualTo("h/Jimi Hendrix/Live At The Hollywood Bowl/"));		
+				},
+				jsonAudioFile -> {
+					JsonNode locationsJson = jsonAudioFile.get( JsonMusicProperties.LOCATION);
+					assertThat(locationsJson).isNotNull().singleElement().satisfies(locationJson -> 
+						assertThat(locationJson.asText()).isEqualTo("h/Jimi Hendrix/The Making Of Electric Ladyland/"));		
+				});
+		
+		JsonNode jsonVideoFiles = formatJson.get(JsonMusicProperties.VIDEO_FILE);
+		assertThat(jsonVideoFiles).isNotNull();
+		assertThat(jsonVideoFiles.size()).isEqualTo(1);
+		
+		JsonNode jsonVideoFile = jsonVideoFiles.get(0);
+		JsonNode locationsJson = jsonVideoFile.get( JsonMusicProperties.LOCATION);
+		assertThat(locationsJson).isNotNull().singleElement().satisfies(locationJson -> 
+			assertThat(locationJson.asText()).isEqualTo("h/Jimi Hendrix/Electric Ladyland 1968/"));	
+	}
+	
 	private JsonNode assertAndGetFormatJson(String albumStr) throws JsonMappingException, JsonProcessingException {
 		
 		AlbumVersionMigrator4 migrator = AlbumVersionMigrator4.getInstance();
