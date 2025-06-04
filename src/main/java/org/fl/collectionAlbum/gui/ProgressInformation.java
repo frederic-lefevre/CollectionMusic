840e3684ccc2598456b3765416bf18b0a2f82c4b
Frederic Lefevre <frederic.bn.lefevre@googlemail.com>
2025-06-04 22:30:47 +0200

    Code format

--- src/main/java/org/fl/collectionAlbum/gui/ProgressInformation.java
+++ src/main/java/org/fl/collectionAlbum/gui/ProgressInformation.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
 
-Copyright (c) 2017, 2023 Frederic Lefevre
+Copyright (c) 2017, 2025 Frederic Lefevre
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -29,7 +29,7 @@ public class ProgressInformation {
 	private String information;
 	
 	public ProgressInformation(String info) {
-		information 	  = info ;
+		information = info;
 	}
 
 	public String getInformation() {
