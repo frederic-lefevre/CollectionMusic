6c8f41e0691635aabad9d9748f6aaa2d022987fb
Frederic Lefevre <frederic.bn.lefevre@googlemail.com>
2026-01-10 18:30:46 +0100

    Update license year

--- src/test/java/org/fl/collectionAlbum/albums/ListeAlbumTest.java
+++ src/test/java/org/fl/collectionAlbum/albums/ListeAlbumTest.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
 
-Copyright (c) 2017, 2025 Frederic Lefevre
+Copyright (c) 2017, 2026 Frederic Lefevre
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -26,6 +26,8 @@ package org.fl.collectionAlbum.albums;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.fl.collectionAlbum.format.Format;
 import org.fl.collectionAlbum.format.MediaSupportCategories;
 import org.junit.jupiter.api.Test;
@@ -49,7 +51,113 @@ class ListeAlbumTest {
 		
 		assertThat(format.getContentNatures()).isEmpty();
 		assertThat(format.getSupportsPhysiques()).isNotNull().containsOnly(MediaSupportCategories.values());
-		assertThat(format.getSupportsPhysiquesNumbers()).allSatisfy((sp, number) -> assertThat(number).isZero());
+		assertThat(format.getSupportsPhysiquesNumbers()).allSatisfy((sp, number) -> assertThat(number).isZero());	
+	}
+	
+	@Test
+	void testEmptyListe2() {
 		
+		ListeAlbum emptyListe =  ListeAlbum.Builder.getBuilderFrom(new ArrayList<>()).build();
+		
+		assertThat(emptyListe).isNotNull();
+		
+		assertThat(emptyListe.getAlbums()).isNotNull().isEmpty();
+		assertThat(emptyListe.getNombreAlbums()).isZero();
+		
+		Format format = emptyListe.getFormatListeAlbum();
+		
+		assertThat(format.hasError()).isFalse();
+		assertThat(format.getPoids()).isZero();
+		
+		assertThat(format.getContentNatures()).isEmpty();
+		assertThat(format.getSupportsPhysiques()).isNotNull().containsOnly(MediaSupportCategories.values());
+		assertThat(format.getSupportsPhysiquesNumbers()).allSatisfy((sp, number) -> assertThat(number).isZero());	
+	}
+	
+	private static final String albumStr1 = """
+			{ 
+			  "titre": "Portrait in jazz",
+			  "format": {  "cd": 1,
+				"audioFiles" : [{
+				    "bitDepth": 16 , 
+				    "samplingRate" : 44.1, 
+				    "source" : "MOFI Fidelity Sound Lab", 
+				    "type" : "FLAC" }]
+			     }, 
+			  "auteurCompositeurs": [ 
+			    {  
+			      "nom": "Evans",
+			      "prenom": "Bill", 
+			      "naissance": "1929-08-16",
+			      "mort": "1980-09-15"  
+			    }                                 
+			  ],    								
+			  "enregistrement": [ "1959-12-28",  "1959-12-28" ],  
+			  "liensUrl":  [ "/Concerts/2006/EricClapton20060505" ] 
+			 } 
+			""" ;
+	
+	private static final String albumStr2 = """
+{ 
+  "titre": "Van Halen",
+  "format": {
+    "cd": 1,
+    "audioFiles": [
+      {
+        "bitDepth": 24,
+        "samplingRate": 192,
+        "source": "File",
+        "type": "FLAC"
+      }
+    ]
+  },
+  "groupe": [
+    {
+      "nom": "Van Halen"
+    }
+  ],
+  "enregistrement": [
+    "1977-09-01",
+    "1977-11-01"
+  ]
+}
+			""" ;
+	
+	private static final String albumStr3 = """
+{
+  "titre": "Magical mystery tour",
+  "format": {
+    "cd": 1,
+    "audioFiles": [
+      {
+        "bitDepth": 16,
+        "samplingRate": 44.1,
+        "source": "CD",
+        "type": "FLAC",
+        "location": [
+          "b/The Beatles/Magical Mystery Tour"
+        ]
+      }
+    ]
+  },
+  "groupe": [
+    {
+      "nom": "Beatles",
+      "article": "The"
+    }
+  ],
+  "enregistrement": [
+    "1966-11-24",
+    "1967-11-07"
+  ],
+  "jsonVersion": 4
+}
+			""";
+	
+
+	@Test
+	void testListeBuild1() {
+		
+		ListeAlbum listeAlbum =  ListeAlbum.Builder.getBuilder().build();
 	}
 }
