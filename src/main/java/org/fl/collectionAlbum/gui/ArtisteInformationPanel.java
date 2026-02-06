58917a4ced790922f560008ca7314788b1a72862
Frederic Lefevre <frederic.bn.lefevre@googlemail.com>
2026-02-06 15:09:35 +0100

    Display name and dates

--- src/main/java/org/fl/collectionAlbum/gui/ArtisteInformationPanel.java
+++ src/main/java/org/fl/collectionAlbum/gui/ArtisteInformationPanel.java
@@ -25,11 +25,15 @@ SOFTWARE.
 package org.fl.collectionAlbum.gui;
 
 import java.awt.Dimension;
+import java.awt.Font;
 import java.util.List;
 
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
+import javax.swing.border.EmptyBorder;
 
 import org.fl.collectionAlbum.albums.Album;
 import org.fl.collectionAlbum.artistes.Artiste;
@@ -39,8 +43,30 @@ public class ArtisteInformationPanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 
+	private static final Font fontNom = new Font("Verdana", Font.BOLD, 18);
+	private static final Font fontDates = new Font("Verdana", Font.BOLD, 14);
+	
 	public ArtisteInformationPanel(Artiste artiste, GenerationPane generationPane) {
-			
+		
+		super();
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		
+		JPanel nomEtDatePanel = new JPanel();
+		nomEtDatePanel.setLayout(new BoxLayout(nomEtDatePanel, BoxLayout.X_AXIS));
+		nomEtDatePanel.setBorder(new EmptyBorder(10, 0, 10, 0));
+		
+		JLabel nomLabel = new JLabel();
+		nomLabel.setFont(fontNom);
+		nomLabel.setText(artiste.getNomComplet());
+		nomEtDatePanel.add(nomLabel);
+		
+		JLabel dateLabel = new JLabel();
+		dateLabel.setFont(fontDates);
+		dateLabel.setText(artiste.getDates());
+		nomEtDatePanel.add(dateLabel);
+		
+		add(nomEtDatePanel);
+		
 		JScrollPane albumsScrollTable = getAlbumsScrollPane(artiste.getAlbums().getAlbums(), generationPane);
 		JScrollPane concertScrollTable = getConcertsScrollPane(artiste.getConcerts().getConcerts());
 		if (concertScrollTable == null) {
