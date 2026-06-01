150e1ba0ffc22dc300d6f29f7a1d018bff290d0e
Frederic Lefevre <frederic.bn.lefevre@googlemail.com>
2026-06-01 11:06:49 +0200

    Use GridBagLayOut

--- src/main/java/org/fl/collectionAlbum/gui/MediaFileDetailPane.java
+++ src/main/java/org/fl/collectionAlbum/gui/MediaFileDetailPane.java
@@ -25,8 +25,11 @@ SOFTWARE.
 package org.fl.collectionAlbum.gui;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.GridLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.util.Map;
 
 import javax.swing.BorderFactory;
@@ -35,7 +38,6 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
-import org.fl.collectionAlbum.Control;
 import org.fl.collectionAlbum.mediaFile.MediaFile;
 import org.fl.collectionAlbum.mediaFile.metadata.MediaFileMetadata;
 import org.fl.collectionAlbum.mediaFile.metadata.MetadataElement;
@@ -45,18 +47,37 @@ public class MediaFileDetailPane extends JScrollPane {
 	private static final long serialVersionUID = 1L;
 
 	private static final Font verdana = new Font("Verdana", Font.BOLD, 14);
+	private static final Dimension metadataWindowDimension = new Dimension(1200, 900);
 	
 	public MediaFileDetailPane(MediaFile mediaFile) {
 		
 		super();
-		setPreferredSize(Control.getInfoWindowDimension());
+		setPreferredSize(metadataWindowDimension);
 		JPanel infosPane = new JPanel();
 		infosPane.setLayout(new BoxLayout(infosPane, BoxLayout.Y_AXIS));
+		infosPane.setAlignmentX(LEFT_ALIGNMENT);
 
-		infosPane.add(descriptionValuePanel("Chemin du fichier", mediaFile.getFilePath().toString()));
-		infosPane.add(descriptionValuePanel("Taille du fichier", mediaFile.getSize().map(v -> Long.toString(v)).orElse("Inconnue")));
-		infosPane.add(descriptionValuePanel("Fichier media valide", mediaFile.isValidMediaFile().map(v -> Boolean.toString(v)).orElse("Inconnu")));
-		infosPane.add(descriptionValuePanel("Image incluse dans le fichier", mediaFile.hasImbeddedPicture().map(v -> Boolean.toString(v)).orElse("Inconnu")));
+		JPanel generalInfoPane = new JPanel();
+		generalInfoPane.setLayout(new GridBagLayout());
+		
+		GridBagConstraints gridConstraints = new GridBagConstraints();
+		gridConstraints.insets =  new Insets(3, 3, 3, 3);
+		gridConstraints.gridx = 0;
+		gridConstraints.gridy = 0;
+		gridConstraints.anchor = GridBagConstraints.LINE_START;
+		
+		descriptionValuePanel(generalInfoPane, gridConstraints, "Chemin du fichier", mediaFile.getFilePath().toString());
+		gridConstraints.gridx = 0;
+		gridConstraints.gridy++;
+		descriptionValuePanel(generalInfoPane, gridConstraints, "Taille du fichier", mediaFile.getSize().map(v -> Long.toString(v)).orElse("Inconnue"));
+		gridConstraints.gridx = 0;
+		gridConstraints.gridy++;
+		descriptionValuePanel(generalInfoPane, gridConstraints, "Fichier media valide", mediaFile.isValidMediaFile().map(v -> Boolean.toString(v)).orElse("Inconnu"));
+		gridConstraints.gridx = 0;
+		gridConstraints.gridy++;
+		descriptionValuePanel(generalInfoPane, gridConstraints, "Image incluse dans le fichier", mediaFile.hasImbeddedPicture().map(v -> Boolean.toString(v)).orElse("Inconnu"));
+		
+		infosPane.add(generalInfoPane);
 		
 		MediaFileMetadata metadata = mediaFile.getMetadata();
 		if (metadata != null) {
@@ -74,33 +95,51 @@ public class MediaFileDetailPane extends JScrollPane {
 	private static JPanel metadataPanel(String titre, Map<String, MetadataElement<?>> metadataMap) {
 
 		JPanel metadataPane = new JPanel();
-		metadataPane.setLayout(new BoxLayout(metadataPane, BoxLayout.Y_AXIS));
+		metadataPane.setLayout(new GridBagLayout());
+		metadataPane.setAlignmentX(RIGHT_ALIGNMENT);
+		
 		metadataPane.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.BLACK));
 
+		GridBagConstraints gridConstraints = new GridBagConstraints();
+		gridConstraints.insets =  new Insets(3, 10, 3, 20);
+		gridConstraints.gridx = 0;
+		gridConstraints.gridy = 0;
+		gridConstraints.gridheight = 1;
+		gridConstraints.gridwidth = 3;
+		gridConstraints.anchor = GridBagConstraints.LINE_START;
+		
 		JLabel titreMetadata = new JLabel(titre);
-		metadataPane.add(titreMetadata);
-
+		titreMetadata.setFont(verdana);
+		
 		if ((metadataMap == null) ||  metadataMap.isEmpty()) {
-			metadataPane.add(new JLabel("Pas de metadata" + titre));
+			titreMetadata.setText("Pas de " + titre);
+			metadataPane.add(titreMetadata, gridConstraints);
 		} else {
+			metadataPane.add(titreMetadata, gridConstraints);
 			metadataMap.values().stream()
-					.forEach(m -> metadataPane.add(descriptionValuePanel(m.name(), m.value().toString())));
+					.forEachOrdered(m -> {
+						gridConstraints.gridx = 0;
+						gridConstraints.gridy++;
+						descriptionValuePanel(metadataPane, gridConstraints, m.name(), m.value().toString());
+					});
 		}
 		return metadataPane;
 	}
 	
-	private static JPanel descriptionValuePanel(String description, String value) {
-		
-		JPanel dvPane = new JPanel();
-		dvPane.setLayout(new GridLayout(1, 2));
-		dvPane.setFont(verdana);	
+	private static void descriptionValuePanel(JPanel pane, GridBagConstraints gridConstraints, String description, String value) {
+	
+		gridConstraints.gridheight = 1;
 		JLabel titreLbl = new JLabel(description + ": ");
 		titreLbl.setFont(verdana);
-		dvPane.add(titreLbl);
+		
+		gridConstraints.gridwidth = 1;
+		gridConstraints.weightx =0;
+		pane.add(titreLbl, gridConstraints);
 		JLabel valueLbl = new JLabel(value);
 		valueLbl.setFont(verdana);
-		dvPane.add(valueLbl);
-		
-		return dvPane;
+		gridConstraints.gridx++;
+		gridConstraints.gridwidth = 2;
+		gridConstraints.weightx =1;
+		pane.add(valueLbl, gridConstraints);
 	}
 }
