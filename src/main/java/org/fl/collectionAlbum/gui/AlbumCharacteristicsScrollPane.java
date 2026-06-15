0bf2c6893cfce55d96c3e619ab77c02e0b9c6583
Frederic Lefevre <frederic.bn.lefevre@googlemail.com>
2026-06-15 08:01:43 +0200

    Better look

--- src/main/java/org/fl/collectionAlbum/gui/AlbumCharacteristicsScrollPane.java
+++ src/main/java/org/fl/collectionAlbum/gui/AlbumCharacteristicsScrollPane.java
@@ -24,18 +24,22 @@ SOFTWARE.
 
 package org.fl.collectionAlbum.gui;
 
-
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.util.Arrays;
 
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.border.EtchedBorder;
 
 import org.fl.collectionAlbum.CollectionAlbumContainer;
 import org.fl.collectionAlbum.albums.ListeAlbum;
+import org.fl.collectionAlbum.format.ContentNature;
 import org.fl.collectionAlbum.gui.listener.ListAlbumListener;
 
 public class AlbumCharacteristicsScrollPane extends JScrollPane implements UpdatableElement  {
@@ -46,6 +50,10 @@ public class AlbumCharacteristicsScrollPane extends JScrollPane implements Updat
 	private static final Font FONT = new Font("Verdana", Font.BOLD, 16);
 	
 	private static final String AUDIO_CHARACTERISTICS_TITLE = "Caractéristiques des numérisations des albums";
+	private static final String CONTENT_NATURE_CHARACTERISTICS_TITLE = "Albums par nature de contenu";
+	
+	private static final Dimension AUDIO_CHARACTERISTICS_DIMENSION = new Dimension(800,500);
+	private static final Dimension CONTENT_NATURE_DIMENSION = new Dimension(800,320);
 	
 	private final CollectionAlbumContainer collectionAlbumContainer;
 	private final GenerationPane generationPane;
@@ -58,7 +66,7 @@ public class AlbumCharacteristicsScrollPane extends JScrollPane implements Updat
 		this.generationPane = generationPane;
 		
 		albumsCaracteristicsPanel = new JPanel();
-		albumsCaracteristicsPanel.setLayout(new BoxLayout(albumsCaracteristicsPanel, BoxLayout.Y_AXIS));
+		albumsCaracteristicsPanel.setLayout(new BoxLayout(albumsCaracteristicsPanel, BoxLayout.X_AXIS));
 		
 		fillPanel();
 		
@@ -68,27 +76,65 @@ public class AlbumCharacteristicsScrollPane extends JScrollPane implements Updat
 	private void fillPanel() {
 		
 		albumsCaracteristicsPanel.removeAll();
+		
+		JPanel albumsAudioPanel = new JPanel();
+		albumsAudioPanel.setLayout(new BoxLayout(albumsAudioPanel, BoxLayout.Y_AXIS));
+		albumsAudioPanel.setBorder(BorderFactory.createCompoundBorder(
+				BorderFactory.createEtchedBorder(EtchedBorder.RAISED), 
+				BorderFactory.createEmptyBorder(40, 30, 40, 30)));
+		albumsAudioPanel.setPreferredSize(AUDIO_CHARACTERISTICS_DIMENSION);
+		
+		JPanel pLabel = new JPanel();
 		JLabel albumsAudioCharacteristicsLabel = new JLabel(AUDIO_CHARACTERISTICS_TITLE);
 		albumsAudioCharacteristicsLabel.setFont(TITLE_FONT);
+		pLabel.add(albumsAudioCharacteristicsLabel);
 		
 		JPanel albumsAudioCharacteristicsPanel = new JPanel();
-		albumsAudioCharacteristicsPanel.setLayout(new GridLayout(0, 2, 50, 50));
+		albumsAudioCharacteristicsPanel.setLayout(new GridLayout(0, 2, 50, 60));
 		
 		ListeAlbum albumsWithAudio = collectionAlbumContainer.getAlbumsWithAudioFile().sortRangementAlbum();
-		addCharacteristic("Albums avec fichier audio", albumsWithAudio, albumsAudioCharacteristicsPanel);
+		addCharacteristic("Albums avec fichiers audio", albumsWithAudio, albumsAudioCharacteristicsPanel);
 		ListeAlbum highResAudioAlbums = collectionAlbumContainer.getAlbumsWithHighResAudio().sortRangementAlbum();
-		addCharacteristic("Albums avec fichier audio haute résolution", highResAudioAlbums, albumsAudioCharacteristicsPanel);
+		addCharacteristic("Albums avec fichiers audio haute résolution", highResAudioAlbums, albumsAudioCharacteristicsPanel);
 		ListeAlbum lowResAudioAlbums = collectionAlbumContainer.getAlbumsWithLowResAudio().sortRangementAlbum();
-		addCharacteristic("Albums avec fichier audio avec perte (basse qualité)", lowResAudioAlbums, albumsAudioCharacteristicsPanel);
+		addCharacteristic("Albums avec fichiers audio avec perte (basse qualité)", lowResAudioAlbums, albumsAudioCharacteristicsPanel);
 		ListeAlbum missingAudioAlbums = collectionAlbumContainer.getAlbumsMissingAudioFile().sortRangementAlbum();
 		addCharacteristic("Albums manquant de fichier audio", missingAudioAlbums, albumsAudioCharacteristicsPanel);
 		ListeAlbum albumsWithVideoFile = collectionAlbumContainer.getAlbumsWithVideoFile().sortRangementAlbum();
-		addCharacteristic("Albums avec fichier vidéo", albumsWithVideoFile, albumsAudioCharacteristicsPanel);
+		addCharacteristic("Albums avec fichiers vidéo", albumsWithVideoFile, albumsAudioCharacteristicsPanel);
 		ListeAlbum albumsMissingVideoFile = collectionAlbumContainer.getAlbumsMissingVideoFile().sortRangementAlbum();
 		addCharacteristic("Albums manquant de fichier vidéo", albumsMissingVideoFile, albumsAudioCharacteristicsPanel);
 		
-		albumsCaracteristicsPanel.add(albumsAudioCharacteristicsLabel);
-		albumsCaracteristicsPanel.add(albumsAudioCharacteristicsPanel);
+		albumsAudioPanel.add(pLabel);
+		albumsAudioPanel.add(albumsAudioCharacteristicsPanel);
+		
+		JPanel albumContentNaturePanel = new JPanel();
+		albumContentNaturePanel.setLayout(new BoxLayout(albumContentNaturePanel, BoxLayout.Y_AXIS));
+		albumContentNaturePanel.setBorder(BorderFactory.createCompoundBorder(
+				BorderFactory.createEtchedBorder(EtchedBorder.RAISED), 
+				BorderFactory.createEmptyBorder(40, 30, 40, 30)));
+		albumContentNaturePanel.setPreferredSize(CONTENT_NATURE_DIMENSION);
+		
+		JPanel pLabel2 = new JPanel();
+		JLabel albumsContentNatureLabel = new JLabel(CONTENT_NATURE_CHARACTERISTICS_TITLE);
+		albumsContentNatureLabel.setFont(TITLE_FONT);
+		pLabel2.add(albumsContentNatureLabel);
+		
+		JPanel albumsContentNatureCharacteristicsPanel = new JPanel();
+		albumsContentNatureCharacteristicsPanel.setLayout(new GridLayout(0, 2, 50, 150));
+		
+		Arrays.stream(ContentNature.values()).forEachOrdered(contentNature -> {
+			ListeAlbum albumsWithOnlyContentNature = collectionAlbumContainer.getAlbumsWithOnlyContentNature(contentNature).sortRangementAlbum();
+			addCharacteristic("Albums avec seulement du contenu " + contentNature.getNom(), albumsWithOnlyContentNature, albumsContentNatureCharacteristicsPanel);
+		});
+		ListeAlbum albumsWithMixedContentNature = collectionAlbumContainer.getAlbumsWithMixedContentNature().sortRangementAlbum();
+		addCharacteristic("Albums avec plusieurs types de contenus", albumsWithMixedContentNature, albumsContentNatureCharacteristicsPanel);
+		
+		albumContentNaturePanel.add(pLabel2);
+		albumContentNaturePanel.add(albumsContentNatureCharacteristicsPanel);
+		
+		albumsCaracteristicsPanel.add(albumsAudioPanel);
+		albumsCaracteristicsPanel.add(albumContentNaturePanel);
 	}
 	
 	private void addCharacteristic(String characteristicTitle, ListeAlbum albums, JPanel panel) {
@@ -103,8 +149,6 @@ public class AlbumCharacteristicsScrollPane extends JScrollPane implements Updat
 	
 	@Override
 	public void updateElement() {
-		fillPanel();
-		
+		fillPanel();		
 	}
-
 }
--- src/main/java/org/fl/collectionAlbum/gui/table/AlbumsTableModel.java
+++ src/main/java/org/fl/collectionAlbum/gui/table/AlbumsTableModel.java
@@ -45,6 +45,4 @@ public class AlbumsTableModel extends AbstractCollectionTableModel<Album> implem
 	public void updateElement() {
 		fireTableDataChanged();		
 	}
-
-
 }
